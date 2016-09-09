db5f0082dea6a52007fdd0ef62921fee134819f0
wuying8624 <114622258@qq.com>
2016-09-09 17:33:06 +0800

    333

--- src/com/Persion.java
+++ src/com/Persion.java
@@ -1,5 +1,8 @@
 package com;
 
 public class Persion {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(11);
+	}
 }
--- src/com/Test.java
+++ src/com/Test.java
@@ -7,6 +7,7 @@ public class Test {
 		System.out.println(11);
 		System.out.println(22);
 		System.out.println(33);
+		System.out.println(44);
 	}
 
 }
